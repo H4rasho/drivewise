00b9d37c6662144fd1edb911a14324bffe8a04e4
h4rasho <h4rasho@gmail.com>
2026-03-22 16:07:33 -0300

    change log

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,7 @@ import { env } from "./shared/lib/env";
 
 const app = createApp();
 
-console.log(`DriveWise API running on port ${env.API_PORT}`);
+console.log(`DRIVEWISE API running on port ${env.API_PORT}`);
 
 export default {
   port: env.API_PORT,
